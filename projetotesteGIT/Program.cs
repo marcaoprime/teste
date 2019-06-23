6a20c27ed66373642075c60250ca905ad4e64525
Marco Antonio <marcobugatudo@gmail.com>
2019-06-23 08:43:47 -0300

    Primeira Versão

---
+++ projetotesteGIT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projetotesteGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
