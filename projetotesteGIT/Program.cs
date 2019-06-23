82645d026b28f345c3317f8929be0ddc6156cf51
Marco Antonio <marcobugatudo@gmail.com>
2019-06-23 08:46:00 -0300

    Alterado

--- projetotesteGIT/Program.cs
+++ projetotesteGIT/Program.cs
@@ -7,6 +7,7 @@ namespace projetotesteGIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Alterado");
             Console.ReadLine();
         }
     }
